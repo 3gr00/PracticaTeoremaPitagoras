f3db3c047e728454feea59b37f20af2707ee8bf8
3gr00 <154273129+3gr00@users.noreply.github.com>
2025-05-20 16:30:18 +0200

    subida clase TrianguloRectangulo

---
+++ PracticaTeoremaPitagoras/src/clases/TrianguloRectangulo.java
@@ -0,0 +1,105 @@
+package clases;
+
+public class TrianguloRectangulo {
+
+	//Cateto 1
+	double a;
+	//Cateto 2
+	double b;
+	//hipotenusa
+	double c;
+	
+	/**
+	 * Constructor por defecto 
+	 */
+	
+	public TrianguloRectangulo () {
+		
+		this.a = 1;
+		this.b = 1;
+		this.c = calcularHipotenusa();
+		
+	}
+	
+	/**
+	 * Constructor que recibe 2 catetos
+	 * @param a cateto 1
+	 * @param b cateto 2
+	 */
+	
+	public TrianguloRectangulo (double a, double b) {
+		
+	        if (a <= 0 || b <= 0) {
+	            throw new IllegalArgumentException("Los catetos deben ser mayores que cero.");
+	        }else {
+	        	this.a = a;
+		        this.b = b;
+		        this.c = calcularHipotenusa();
+	    }
+
+	}
+	
+	/**
+	 * Calcular la Hipotenusa
+	 * @return Hipotenusa
+	 */
+	
+	public double calcularHipotenusa () {
+		
+		return  Math.sqrt((a*a) + (b*b));
+		
+	}
+	
+	/**
+	 * Calcular area
+	 * @return area
+	 */
+	
+	public double area() {
+		
+        return (a * b) / 2;
+        
+    }
+	
+	/**
+	 * Calcular Perimetro
+	 * @return Perimetro
+	 */
+	
+	public double perimetro() {
+		 
+		return a + b + c;
+	        
+	    }
+
+
+	//SETTERS
+	
+	public void setA(double a) {
+        if (a <= 0) throw new IllegalArgumentException("El cateto a debe ser mayor que cero.");
+        this.a = a;
+        calcularHipotenusa();
+    }
+	
+	public void setB(double b) {
+        if (a <= 0) throw new IllegalArgumentException("El cateto b debe ser mayor que cero.");
+        this.b = b;
+        calcularHipotenusa();
+    }
+	
+	//TOSTRING
+	
+	public String toString () {
+		
+		String mensaje = "";
+		
+		mensaje += "Cateto 1: " + this.a + "\n";
+		mensaje += "Cateto 2: " + this.b + "\n";
+		mensaje += "Hipotenusa : " + this.c + "\n";
+		
+		return mensaje;
+		
+	}
+
+	
+}
